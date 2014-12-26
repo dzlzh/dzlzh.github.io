d41dbb6db906bce97270f02b5a822336b49eb52f
dzl <89718843@qq.com>
2014-12-26 16:08:00 +0800

    snow

--- js/jquery.snow.min.1.0.js
+++ js/jquery.snow.min.1.0.js
@@ -1,19 +1,46 @@
 /**
- * jquery.snow - jQuery Snow Effect Plugin
- *
- * Available under MIT licence
- *
- * @version 1 (21. Jan 2012)
- * @author Ivan Lazarevic
- * @requires jQuery
- * @see http://workshop.rs
- *
- * @params minSize - min size of snowflake, 10 by default
- * @params maxSize - max size of snowflake, 20 by default
- * @params newOn - frequency in ms of appearing of new snowflake, 500 by default
- * @params flakeColor - color of snowflake, #FFFFFF by default
- * @example $.fn.snow({ maxSize: 200, newOn: 1000 });
- */
+* js网页雪花效果jquery插件 
+* 柯乐义 keleyi.com 整理
+* http://keleyi.com/a/e8t7hoj4.htm
+*/
+(function ($) {
 
-(function($){$.fn.snow=function(options){var $flake=$('<div id="flake" />').css({'position':'absolute','top':'-50px'}).html('&#10052;'),documentHeight=$(document).height(),documentWidth=$(document).width(),defaults={minSize:10,maxSize:20,newOn:500,flakeColor:"#FFFFFF"},options=$.extend({},defaults,options);var interval=setInterval(function(){var startPositionLeft=Math.random()*documentWidth-100,startOpacity=0.5+Math.random(),sizeFlake=options.minSize+Math.random()*options.maxSize,endPositionTop=documentHeight-40,endPositionLeft=startPositionLeft-100+Math.random()*200,durationFall=documentHeight*10+Math.random()*5000;$flake.clone().appendTo('body').css({left:startPositionLeft,opacity:startOpacity,'font-size':sizeFlake,color:options.flakeColor}).animate({top:endPositionTop,left:endPositionLeft,opacity:0.2},durationFall,'linear',function(){$(this).remove()});},options.newOn);};})(jQuery);
+    $.fn.snow = function (options) {
 
+        var $flake = $('<div id="snowbox-k' + 'eleyi-com" />').css({ 'position': 'absolute', 'top': '-50px' }).html('&#10052;'),
+documentHeight = $(document).height(),
+documentWidth = $(document).width(),
+defaults = {
+    minSize: 10,  //雪花的最小尺寸
+    maxSize: 20,  //雪花的最大尺寸
+    newOn: 1000,  //雪花出现的频率
+    flakeColor: "#FFFFFF"	//柯乐义 keleyi.com 整理
+},
+options = $.extend({}, defaults, options);
+
+        var interval = setInterval(function () {
+            var startPositionLeft = Math.random() * documentWidth - 100,
+startOpacity = 0.5 + Math.random(),
+sizeFlake = options.minSize + Math.random() * options.maxSize,
+endPositionTop = documentHeight - 40,
+endPositionLeft = startPositionLeft - 100 + Math.random() * 500,
+durationFall = documentHeight * 10 + Math.random() * 5000;
+            $flake.clone().appendTo('body').css({
+                left: startPositionLeft,
+                opacity: startOpacity,
+                'font-size': sizeFlake,
+                color: options.flakeColor
+            }).animate({
+                top: endPositionTop,
+                left: endPositionLeft,
+                opacity: 0.2
+            }, durationFall, 'linear', function () {
+                $(this).remove()
+            }
+);
+
+        }, options.newOn);
+
+    };
+
+})(jQuery);
